23cb9a5c50fa5ed2cd7b60dfc1d413ae3b9cf955
Muiz Oyebowale <106952550+Muizzyranking@users.noreply.github.com>
2022-07-05 22:33:22 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@
 
 void print_times_table(int n)
 {
-	int prod, mult, num;
+int prod, mult, num;
 
 	if (n <= 15 && n >= 0)
 	{
@@ -33,7 +33,9 @@ void print_times_table(int n)
 					_putchar((prod / 10) % 10 + 48);
 				} else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48); 
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
+	}
+}
