409ed0afcfaeb8ce5ab1b09d857c7b13610c0697
Muiz Oyebowale <106952550+Muizzyranking@users.noreply.github.com>
2022-07-05 23:44:53 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,13 +4,13 @@
  * main - finds and prints the sum of the even-valued terms
  *  * followed by a new line
  * Return: Always 0 (Success)
- * 
+ *
  */
 int main(void)
 {
 	int i;
 	unsigned long int j, k, next, sum;
-	
+
 	j = 1;
 	k = 2;
 	sum = 0;
